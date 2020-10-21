023fb2792995250d517c3081455aaa04c5813f2a
HossamElharmeil <hossam.elharmil@gmail.com>
2020-10-22 01:18:35 +0200

    fill-grid: add functions isInRow and isInColumn

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,5 +1,7 @@
 import { GRID, NUMBERS } from 'typings'
 import shuffle from 'utils/shuffle'
+import isInCol from './is-in-col'
+import isInRow from './is-in-row'
 
 const grid = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,7 +24,7 @@ const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
  */
 function fillGrid(grid: GRID) {
   let row = 0
-  let col = 9
+  let col = 0
 
   for (let i = 0; i < 81; i++) {
     row = Math.floor(i / 9)
@@ -30,11 +32,23 @@ function fillGrid(grid: GRID) {
 
     if (grid[row][col] === 0) {
       shuffle(numbers)
+
+      for (let value of numbers) {
+        if (!isInRow({ grid, row, value })) {
+          if (!isInCol({ grid, col, value })) {
+            grid[row][col] = value
+          }
+        }
+        //check grid if it is full, if yes, stop and ruturn true
+        //otherwise fillGrid(grid)
+      }
       //TODO: do stuff
       //TODO: recursive things
       break
     }
   }
+
+  grid[col][row] = 0
 }
 
 export default fillGrid
---
+++ src/utils/fill-grid/is-in-row/index.ts
@@ -0,0 +1,17 @@
+import { GRID, NUMBERS } from 'typings'
+
+interface IInput {
+  grid: GRID
+  row: number
+  value: NUMBERS
+}
+
+/**
+ * Check if a value exists in a row within a 9x9 grid
+ * @param input Object with 9x9 grid, row index and  a value
+ */
+function isInRow({ grid, row, value }: IInput): boolean {
+  return grid[row].includes(value)
+}
+
+export default isInRow
